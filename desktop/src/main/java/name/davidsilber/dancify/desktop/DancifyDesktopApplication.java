1e496c9cf485e6b76eefba64ae3f28d8651f3bb5
David H. Silber <davidhsilber@gmail.com>
2019-11-19 11:06:31 -0500

    Upgraded to Java 13

--- desktop/src/main/java/name/davidsilber/dancify/desktop/DancifyDesktopApplication.java
+++ desktop/src/main/java/name/davidsilber/dancify/desktop/DancifyDesktopApplication.java
@@ -8,29 +8,26 @@ import org.springframework.context.annotation.Bean;
 
 import java.util.Arrays;
 
-
 @SpringBootApplication
 public class DancifyDesktopApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DancifyDesktopApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(DancifyDesktopApplication.class, args);
+	}
 
+	// This bit swiped from https://spring.io/guides/spring-boot/ just to make sure
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
 
-    // This bit swiped from https://spring.io/guides/spring-boot/ just to make sure 
-    @Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-        return args -> {
+			System.out.println("Let's inspect the beans provided by Spring Boot:");
 
-            System.out.println("Let's inspect the beans provided by Spring Boot:");
+			String[] beanNames = ctx.getBeanDefinitionNames();
+			Arrays.sort(beanNames);
+			for (String beanName : beanNames) {
+				System.out.println(beanName);
+			}
 
-            String[] beanNames = ctx.getBeanDefinitionNames();
-            Arrays.sort(beanNames);
-            for (String beanName : beanNames) {
-                System.out.println(beanName);
-            }
-
-        };
-    }
-
+		};
+	}
 }
